028af2d9ff5c54d6b026025af0473d78220477b4
Hubert Yang <offspring7890@yahoo.com>
2023-04-24 22:25:18 -0700

    change about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -68,13 +68,13 @@ const about = () => {
                   <AnimatedNumbers value="50"/>+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                  Satisfied Clients
+                  Satisfied Employees
                 </h2>
               </div>
 
               <div className='flex flex-col items-end justify-center xl:items-center'>
                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value="40"/>+
+                  <AnimatedNumbers value="20"/>+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
                   Projects Completed
